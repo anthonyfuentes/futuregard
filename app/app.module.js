9a2a3cd7bf73fb23755309a3bf2ed8730ead6553
Pramod Jacob <psjacob68@gmail.com>
2017-01-27 13:06:39 -0500

    working on pseudocode

---
+++ app/app.module.js
@@ -0,0 +1,11 @@
+states
+  default -> ''
+  portfolio -> '/portfolio'
+    content ui view
+    portfolio template
+  trade -> '/trade'
+    content ui view
+    trade form template
+  transactions -> '/transactions'
+    content ui view
+    transactions template
