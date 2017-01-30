842737b95c4c832e17c6a8d3ab5fd6650eb7c09a
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-30 17:33:52 -0500

    Switch from date input to Bootstrap UI element

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,5 @@
-var FG = angular.module('FG', ['ui.router']);
+
+var FG = angular.module('FG', ['ui.router', 'ui.bootstrap']);
 
 FG.run(function($rootScope){
   $rootScope.$on("$stateChangeError", console.log.bind(console));
--- app/components/main/date_selector/dateSelectorCtrl.js
+++ app/components/main/date_selector/dateSelectorCtrl.js
@@ -8,6 +8,16 @@ FG.controller('DateSelectorCtrl',
       $scope.currentDate = dateService.getCurrent();
       $scope.endDate = dateService.getEnd();
 
+      $scope.dateOptions = {
+        minDate: $scope.startDate,
+        maxDate: $scope.endDate,
+        initDate: $scope.currentDate
+      };
+
+      $scope.toggleOpened = function() {
+        $scope.opened = !$scope.opened;
+      };
+
       $scope.changeDate = function(date) {
         var date = new Date(date);
         dateService.setDate(date);
