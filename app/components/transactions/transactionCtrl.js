76c1bc2927844169d04fc9d7ccafda068c8bfdc3
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-30 18:24:34 -0500

    Paginate transactions

--- app/components/transactions/transactionCtrl.js
+++ app/components/transactions/transactionCtrl.js
@@ -4,9 +4,30 @@ FG.controller('TransactionCtrl',
 
     function($scope, transactionService) {
 
-      var transactionHistory = transactionService.getTransactions();
+      var allTransactions = Object.values(transactionService.getTransactions());
 
-      $scope.transactions = Object.values(transactionHistory);
+      $scope.transactions = [];
+
+      $scope.pagination = {
+        currentPage: 1,
+        perPage:  10,
+        numPages: allTransactions.length / 10,
+        maxSize: 5,
+        totalItems: allTransactions.length
+      };
+
+      var _updatePagination = function() {
+        var pagination = $scope.pagination;
+        var startIndex = (pagination.currentPage - 1) * pagination.perPage;
+        var endIndex = startIndex + pagination.perPage;
+        angular.copy(allTransactions.slice(startIndex, endIndex), $scope.transactions);
+      };
+
+      $scope.$watch('pagination.currentPage', function() {
+        _updatePagination();
+      });
+
+      _updatePagination();
 
     }
 
