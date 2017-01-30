62ab62579be279d5f3913c0603fed3b9a1d7fc02
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-30 17:07:03 -0500

    Implement transaction filter

---
+++ app/shared/filters/transactionFilter.js
@@ -0,0 +1,12 @@
+
+FG.filter('transactionFilter', function() {
+
+  return function(transactions, symbol) {
+    if (!symbol || symbol.length === 0) return transactions;
+    symbol = symbol.toLowerCase();
+    return transactions.filter(function(transaction) {
+      return transaction.stock.toLowerCase.startsWith(symbol);
+    });
+  };
+
+})
