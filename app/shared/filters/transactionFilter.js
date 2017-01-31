c8c002e4acbd5cdcb705785ffdbca32665278809
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-30 21:22:36 -0500

    Debug transaction filter

--- app/shared/filters/transactionFilter.js
+++ app/shared/filters/transactionFilter.js
@@ -5,7 +5,7 @@ FG.filter('transactionFilter', function() {
     if (!symbol || symbol.length === 0) return transactions;
     symbol = symbol.toLowerCase();
     return transactions.filter(function(transaction) {
-      return transaction.stock.toLowerCase.startsWith(symbol);
+      return transaction.stock.toLowerCase().startsWith(symbol);
     });
   };
 
