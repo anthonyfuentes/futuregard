d3ac0ab443b58803eb0a739a6caeceee1427ac02
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-01-31 16:34:45 -0500

    Add stats to navbar

--- app/components/main/mainCtrl.js
+++ app/components/main/mainCtrl.js
@@ -1,43 +1,54 @@
 
 FG.controller('MainCtrl',
-  ['$scope', 'allStocks', 'dateService', 'settingService',
+  ['$scope', 'allStocks', 'dateService', 'settingService', 'portfolioService',
 
-  function($scope, allStocks, dateService, settingService) {
+    function($scope, allStocks, dateService, settingService, portfolioService) {
 
-    $scope.allStocks = allStocks;
-    $scope.currentStocks = {};
-    $scope.settings = settingService.getSettings();
+      $scope.allStocks = allStocks;
+      $scope.currentStocks = {};
+      $scope.settings = settingService.getSettings();
+      $scope.stats = {};
 
-    $scope.updateData = function() {
-      $scope.updateStocks();
-      $scope.updateDate();
-    };
+      $scope.updateData = function() {
+        $scope.updateStocks();
+        $scope.updateDate();
+        $scope.updateStats();
+      };
 
-    $scope.updateStocks = function() {
-      var currentDate = dateService.getCurrent();
-      var dateKey = dateService.stringify(currentDate);
-      angular.copy(allStocks[dateKey], $scope.currentStocks)
-    };
+      $scope.updateStocks = function() {
+        var currentDate = dateService.getCurrent();
+        var dateKey = dateService.stringify(currentDate);
+        angular.copy(allStocks[dateKey], $scope.currentStocks)
+      };
 
-    $scope.updateDate = function() {
-      $scope.atEnd = dateService.atEnd();
-      $scope.atStart = dateService.atStart();
-    };
+      $scope.updateDate = function() {
+        $scope.atEnd = dateService.atEnd();
+        $scope.atStart = dateService.atStart();
+      };
 
-    $scope.updateData();
+      $scope.updateStats = function() {
+        $scope.stats.currentCash = portfolioService.getFunds();
+        $scope.stats.daysRemaining = dateService.getDaysRemaining();
+      };
 
-    $scope.nextDay = function() {
-      dateService.nextDay();
-    };
-
-    $scope.previousDay = function() {
-      dateService.previousDay();
-    };
-
-    $scope.$on('dateChange', function() {
       $scope.updateData();
-    });
 
-  }
+      $scope.nextDay = function() {
+        dateService.nextDay();
+      };
+
+      $scope.previousDay = function() {
+        dateService.previousDay();
+      };
+
+      $scope.$on('dateChange', function() {
+        $scope.updateData();
+      });
+
+      $scope.$on('trade', function() {
+        $scope.updateStats();
+      });
+
+    }
 
 ]);
--- app/components/main/primary_navbar/primaryNavbar.js
+++ app/components/main/primary_navbar/primaryNavbar.js
@@ -3,7 +3,10 @@ FG.directive('primaryNavbar', function() {
 
   return {
     templateUrl: 'app/components/main/primary_navbar/primaryNavbar.html',
-    restrict: 'E'
+    restrict: 'E',
+    scope: {
+      stats: '='
+    }
   };
 
 });
